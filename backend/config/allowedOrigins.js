83acd9af581abb6f98e9046077513d7a390de1a3
Soniya Taneja <soniyataneja23@gmail.com>
2026-08-12 15:45:04 +0530

    Allow deployed frontend in CORS

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'https://technotes-nwwc.onrender.com'
 ]
 
 module.exports = allowedOrigins
